5cc78dce2bff9f7249fd7e7782174bf00b5b89a5
AVtyurin <AVtyurin@luxoft.com>
2018-06-22 17:48:26 +0300

    update test

--- src/test/kotlin/net/stits/osen/controller/ExampleController.kt
+++ src/test/kotlin/net/stits/osen/controller/ExampleController.kt
@@ -21,15 +21,14 @@ class ExampleController {
      *      handlePing(b: Address)
      *      handlePing(x: PingPayload, y: Address)
      *      handlePing(sender: Address, payload: PingPayload)
-     * just make sure you passing to it no more than 2 parameters, and sender is always Address (payload can be Any)
+     * just make sure you passing to it no more than 2 parameters, and sender is always Address (payload can be Any?)
      */
     @On(TestMessageTypes.PING)
     fun handlePing(payload: PingPayload, sender: Address) {
         println("RECEIVED PING REQUEST WITH PAYLOAD $payload, SENDING PONG BACK...")
 
-        val addr = Address("localhost", 1337)
         val message = Message(TOPIC_TEST, TestMessageTypes.PONG, "Pong string payload")
-        P2P.send(addr, message, 1337)
+        P2P.send(sender, message, 1337)
     }
 
     @On(TestMessageTypes.PONG)
